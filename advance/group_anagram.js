38acbb210bf932f3bd6d83d6f0fd1d6cafb93e38
Nawin chander <nawinnapzy289@gmail.com>
2026-03-12 08:49:43 +0530

    add changes

--- advance/group_anagram.js
+++ advance/group_anagram.js
@@ -9,3 +9,30 @@
 // ]
 
 
+function groupAnagrams(strs) {
+
+    let map = new Map();
+
+    for (let word of strs) {
+
+        let key = word.split("").sort().join("");
+
+        if (!map.has(key)) {
+            map.set(key, []);
+        }
+
+        map.get(key).push(word);
+    }
+
+    return Array.from(map.values());
+}
+
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]));
+
+
+
+/// Time complexity
+
+//// O(n * k log k)
+
+
